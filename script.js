d7eae13693fb43b7b84796d3e96a0e53d8a61bf2
Alexander Ela <ela.alexander@gmail.com>
2018-07-08 11:11:24 -0600

    Establish JS variables, EVs and function names

---
+++ script.js
@@ -0,0 +1,40 @@
+//Variables
+var webTitleInput = document.querySelector("#title-input");
+var webUrlInput = document.querySelector("#url-input");
+var enterBtn = document.querySelector("#enter-button");
+var titleOutput = document.querySelector("#title-output");
+var urlOutput = document.querySelector("#url-output");
+var readBtn = document.querySelector("#read-button");
+var deleteBtn = document.querySelector("#delete-button");
+
+
+//Event Listeners
+webTitleInput.addEventListener("keyup", enterTitle);
+webUrlInput.addEventListener("keyup", enterUrl);
+enterBtn.addEventListener("click", submit);
+readBtn.addEventListener("click", markRead);
+//second event listner for mark as unread???//
+deleteBtn.addEventListener("click", markDelete);
+
+//Functions
+
+enterTile() {
+
+};
+
+enterUrl() {
+
+};
+
+submit() {
+
+};
+
+markRead() {
+
+};
+
+markDelete() {
+
+};
+
