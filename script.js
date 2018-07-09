aa87f892e94b983a12e822158395f1d9445843c3
Alexander Ela <ela.alexander@gmail.com>
2018-07-08 18:37:00 -0600

    Clean up script.js commented-out notes

--- script.js
+++ script.js
@@ -3,31 +3,13 @@ var webTitleInput = document.querySelector("#title-input");
 var webUrlInput = document.querySelector("#url-input");
 var enterBtn = document.querySelector(".enter-button");
 var bookmarksList = document.querySelector(".bookmarks-list");
-//Redundant
-// var titleOutput = document.querySelector("#title-output");
-// var urlOutput = document.querySelector("#url-output");
 var readBtn = document.querySelector("#read-button");
 var deleteBtn = document.querySelector("#delete-button");
 
 //Event Listeners
-// webTitleInput.addEventListener("keyup", enterTitle);
-// webUrlInput.addEventListener("keyup", enterUrl);
-//Don't need these
 enterBtn.addEventListener("click", submit);
-// readBtn.addEventListener("click", markRead);
-//second event listner for mark as unread???//
-// deleteBtn.addEventListener("click", markDelete);
 
 //Functions
-
-// enterTile() {
-// //redundant
-// };
-
-// enterUrl() {
-// //redundant
-// };
-
 function submit() {
 	event.preventDefault();
 	var bookmarkEntry = `<article aria-label="bookmark">
