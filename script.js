c42733e810d432bf00e07c3ef5697c07a086f472
Alexander Ela <ela.alexander@gmail.com>
2018-07-08 18:11:59 -0600

    Add checkForInput function before merge

--- script.js
+++ script.js
@@ -3,43 +3,33 @@ var webTitleInput = document.querySelector("#title-input");
 var webUrlInput = document.querySelector("#url-input");
 var enterBtn = document.querySelector(".enter-button");
 var bookmarksList = document.querySelector(".bookmarks-list");
-//Redundant
-// var titleOutput = document.querySelector("#title-output");
-// var urlOutput = document.querySelector("#url-output");
 var readBtn = document.querySelector("#read-button");
 var deleteBtn = document.querySelector("#delete-button");
 
 //Event Listeners
-// webTitleInput.addEventListener("keyup", enterTitle);
-// webUrlInput.addEventListener("keyup", enterUrl);
-//Don't need these
 enterBtn.addEventListener("click", submit);
-// readBtn.addEventListener("click", markRead);
-//second event listner for mark as unread???//
-// deleteBtn.addEventListener("click", markDelete);
 
-//Functions
-
-// enterTile() {
-// //redundant
-// };
-
-// enterUrl() {
-// //redundant
-// };
 
 function submit() {
 	event.preventDefault();
+	checkForInputs();
 	var bookmarkEntry = `<article aria-label="bookmark">
 				<h3>${webTitleInput.value}</h3>
 				<div class="link-border">
-				<a href="${webUrlInput.value}" alt="Link to [bookmarked site]">${webUrlInput.value}</a></div>
+				<a href="http://${webUrlInput.value}" alt="Link to [bookmarked site]">${webUrlInput.value}</a></div>
 				<button class="read-button" type="button">Read</button>
 				<button class="delete-button" type="reset">Delete</button>
 				</article>`;
 
-	bookmarksList.innerHTML += bookmarkEntry ;
+	bookmarksList.innerHTML += bookmarkEntry;
 };
+
+function checkForInputs() {
+	
+};
+
+
+
 
 function markRead() {
 
