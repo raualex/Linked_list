22f0e2e9def95f12ba2c5bf3c970c22c0d7656fa
Alexander Ela <ela.alexander@gmail.com>
2018-07-08 20:14:04 -0600

    Add alert requiring user to submit both website title and URL

--- script.js
+++ script.js
@@ -7,21 +7,37 @@ var readBtn = document.querySelector("#read-button");
 var deleteBtn = document.querySelector("#delete-button");
 
 //Event Listeners
-enterBtn.addEventListener("click", submit);
+enterBtn.addEventListener("click", checkForInputs);
 
 //Functions
-function submit() {
-	event.preventDefault();
-	var bookmarkEntry = `<article aria-label="bookmark">
-				<h3>${webTitleInput.value}</h3>
-				<div class="link-border">
-				<a href="${webUrlInput.value}" alt="Link to [bookmarked site]">${webUrlInput.value}</a></div>
-				<button class="read-button" type="button" onclick="markRead(event)">Read</button>
-				<button class="delete-button" type="reset">Delete</button>
-				</article>`;
 
-	bookmarksList.innerHTML += bookmarkEntry ;
+function checkForInputs() {
+	event.preventDefault();
+	if (webTitleInput.value == "" || webUrlInput.value == "") {
+		alert("Please enter both a website title and a valid URL.");
+	}
+	else {
+		submit();
+		console.log("so far so good");
+	}
 };
+
+
+function submit() {
+	var bookmarkEntry = `<article aria-label="bookmark">
+		<h3>${webTitleInput.value}</h3>
+		<div class="link-border">
+		<a href="http://${webUrlInput.value}" alt="Link to ${webTitleInput.value}">${webUrlInput.value}</a></div>
+		<button class="read-button" type="button" onclick="markRead(event)">Read</button>
+		<button class="delete-button" type="reset">Delete</button>
+		</article>`;
+
+	bookmarksList.innerHTML += bookmarkEntry;
+	console.log("nailed it")
+};
+
+
+
 
 function markRead(event) {
   event.preventDefault();
